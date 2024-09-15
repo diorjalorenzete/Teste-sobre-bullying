17f93fe3bc6eca625f878d19de889781494f9da2
diorjalorenzete <128148782+diorjalorenzete@users.noreply.github.com>
2024-09-15 18:52:27 -0300

    Add files via upload

---
+++ script.js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["eu", "eu", "eu",];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
---
+++ script.js/perguntas.js
@@ -0,0 +1,130 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é o tipo mais comum de bullying nas escolas?",
+        alternativas: [
+            {
+                texto: "Bullying Verbal!",
+                afirmacao: [
+                    "O tipo mais comum de bullying inclui insultos e comentários depreciativos, que podem afetar a autoestima da vítima.",
+                    "O bullying pode ser menos visível para os outros, tornando mais difícil para os professores e pais perceberem."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Bullying Físico",
+                afirmacao: [
+                    "O bullying envolve agressões físicas como empurrões ou socos, geralmente mais fácil de detectar.",
+                    "O bullying pode deixar marcas visíveis, mas muitas vezes ocorre em locais onde a supervisão é limitada"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Como os alunos geralmente reagem ao bullying",
+        alternativas: [
+            {
+                texto: " Ignorando",
+                afirmacao: [
+                    "Ao sofrer algum tipo de bullying, o aluno deve imediatamente buscar ajuda.",
+                    "Os alunos que são vítimas de bullying, em grande parte das vezes, ficam com medo ou vergonha de ir para a escola. Ou seja, além das consequências para o lado físico, emocional e psicológico, este tipo de agressão também afeta o desempenho do estudante."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Contando a um adulto",
+                afirmacao: [
+                    "Contar a um adulto pode resultar em intervenção imediata e suporte, ajudando a resolver o problema mais rapidamente.",
+                    "Não se deve ter medo de contar a algum adulto sobre a prática de bullying feita por alguém, pois pode trazer graves consequencias.",
+                   
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o papel dos colegas na luta contra o bullying?",
+        alternativas: [
+            {
+                texto: "Apoiar as vítimas e informar os adultos responsáveis sobre o bullying.",
+                afirmacao: [
+                    "Oferecer apoio às vítimas e relatar o bullying ajuda a criar um ambiente mais seguro e pode parar o comportamento prejudicial.",
+                    "Intervir de maneira segura e informar adultos pode acelerar a resolução do problema e ajudar a vítima a obter ajuda."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Ignorar o problema e não se envolver para evitar conflitos.",
+                afirmacao: [
+                    " Ignorar o bullying pode perpetuar o comportamento e deixar a vítima sem apoio.",
+                    "A falta de intervenção pode contribuir para a continuidade do bullying e aumentar o sofrimento da vítima.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como eu me sinto ao ser criticado por outros?",
+        alternativas: [
+            {
+                texto: "Tento entender a crítica e usá-la para melhorar.",
+                afirmacao: [
+                    " Receber críticas construtivas de forma positiva e buscar melhorar pode ser um sinal de autoconhecimento e abertura ao crescimento pessoal.",
+                    "Essa abordagem pode ajudar a transformar feedback negativo em oportunidades de aprendizado e desenvolvimento."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: " Fico defensivo e ignoro a crítica.",
+                afirmacao: [
+                    "Reagir defensivamente pode ser uma forma de proteger a autoestima, mas pode também dificultar o aprendizado e a melhoria pessoal.",
+                    "Ignorar críticas pode levar à falta de autodesenvolvimento e manter padrões que poderiam ser ajustados."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Quando percebo que alguém está sendo excluído socialmente, o que faço?",
+        alternativas: [
+            {
+                texto: "Procuro incluir a pessoa e promover um ambiente acolhedor.",
+                afirmacao: [
+                    "Fazer um esforço para incluir aqueles que estão sendo excluídos pode refletir empatia e um desejo de criar um ambiente mais inclusivo.",
+                    "Atitudes como essa podem ajudar a combater o bullying e promover uma cultura de apoio e respeito."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Evito a pessoa para não me envolver no problema.",
+                afirmacao: [
+                    "Evitar a situação pode ser uma forma de autoproteção, mas pode também deixar a pessoa excluída sem apoio e contribuir para a perpetuação do problema.",
+                    "Não se envolver pode resultar em sentimentos de desconexão e desamparo tanto para a pessoa excluída quanto para você."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como enfrento situações em que me sinto maltratado ou desrespeitado?",
+        alternativas: [
+            {
+                texto: "Busco expressar meus sentimentos de maneira calma e assertiva.",
+                afirmacao: [
+                    "Expressar sentimentos de forma calma e assertiva pode ajudar a resolver conflitos de maneira construtiva e promover uma comunicação aberta.",
+                    "Essa abordagem pode ajudar a estabelecer limites claros e buscar soluções para problemas de respeito e tratamento."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: " Reajo com raiva ou evito confrontar a situação.",
+                afirmacao: [
+                    "Reagir com raiva pode intensificar o conflito e dificultar a resolução de problemas, enquanto evitar o confronto pode deixar a situação sem solução.",
+                    "Essas reações podem refletir dificuldades em lidar com conflitos e sentimentos negativos, impactando a resolução efetiva da situação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    
+];
